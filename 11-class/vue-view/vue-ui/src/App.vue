15067ed34d4a85e3dc5bc3a750c0e00a360c0d70
xiaowen <583441832@qq.com>
2019-05-27 21:12:52 +0800

    更新

---
+++ 11-class/vue-view/vue-ui/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Button type="primary" icon="ios-person">按钮</Button>
+    <Button type="success">按钮</Button>
+    <Switch v-model="switch1" @on-change="change"/>
+    <Page :total="100" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      switch1: false
+    };
+  },
+  methods: {
+    change(status) {
+      this.$Message.info("开关状态：" + status);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
